975cae7da3b15a1b96bb13fc19a2551a44a2a16e
Joseph Macharia <josephm2800@gmail.com>
2022-04-24 04:24:14 +0300

    finished with right side

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders=[
+    {
+        productName:'Foldable mini drone',
+        productNumber:'63748',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Larvender KF102 Drone',
+        productNumber:'23883',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Ruko F11 Pro Drone',
+        productNumber:'387489',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Drone with camera Drone',
+        productNumber:'56548',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    }
+]
